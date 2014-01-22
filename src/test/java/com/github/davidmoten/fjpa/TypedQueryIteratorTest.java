5f1a78cf3ddf94c2a1273fab730d9bdcb95f7be6
Dave Moten <davidmoten@gmail.com>
2014-01-23 07:49:07 +1100

    add tests

--- src/test/java/com/github/davidmoten/fjpa/TypedQueryIteratorTest.java
+++ src/test/java/com/github/davidmoten/fjpa/TypedQueryIteratorTest.java
@@ -1,5 +1,7 @@
 package com.github.davidmoten.fjpa;
 
+import static com.github.davidmoten.fjpa.Document.toId;
+import static com.github.davidmoten.fjpa.TypedQueryIterator.query;
 import static org.junit.Assert.assertEquals;
 
 import javax.persistence.EntityManager;
@@ -14,19 +16,68 @@ import com.google.common.collect.Lists;
 public class TypedQueryIteratorTest {
 
 	@Test
-	public void testEmf() {
-		EntityManager em = emf().createEntityManager();
+	public void testIteratorReturnsAllDocuments() {
+		EntityManagerFactory emf = emf();
+		EntityManager em = emf.createEntityManager();
+		insertDocuments(em);
+		TypedQuery<Document> q = em.createQuery("from Document order by id",
+				Document.class);
+		assertEquals(Lists.newArrayList("a", "b", "c"), query(q).fluent().transform(toId()).toList());
+		emf.close();
+	}
+	
+	
+	@Test
+	public void testIteratorReturnsPartialListOfDocuments() {
+		EntityManagerFactory emf = emf();
+		EntityManager em = emf.createEntityManager();
+		insertDocuments(em);
+		TypedQuery<Document> q = em.createQuery("from Document where id > 'a' order by id",
+				Document.class);
+		assertEquals(Lists.newArrayList("b", "c"), query(q).fluent().transform(toId()).toList());
+		emf.close();
+	}
+	
+	@Test
+	public void testIteratorReturnsNoDocuments() {
+		EntityManagerFactory emf = emf();
+		EntityManager em = emf.createEntityManager();
+		insertDocuments(em);
+		TypedQuery<Document> q = em.createQuery("from Document where id > 'c' order by id",
+				Document.class);
+		assertEquals(Lists.newArrayList(), query(q).fluent().transform(toId()).toList());
+		emf.close();
+	}
+	
+	@Test
+	public void testIteratorReturnsAllDocumentsUsingTwoPages() {
+		EntityManagerFactory emf = emf();
+		EntityManager em = emf.createEntityManager();
+		insertDocuments(em);
+		TypedQuery<Document> q = em.createQuery("from Document order by id",
+				Document.class);
+		assertEquals(Lists.newArrayList("a", "b", "c"), query(q).pageSize(2).fluent().transform(toId()).toList());
+		emf.close();
+	}
+	
+	@Test
+	public void testIteratorReturnsAllDocumentsUsingThreePages() {
+		EntityManagerFactory emf = emf();
+		EntityManager em = emf.createEntityManager();
+		insertDocuments(em);
+		TypedQuery<Document> q = em.createQuery("from Document order by id",
+				Document.class);
+		assertEquals(Lists.newArrayList("a", "b", "c"), query(q).pageSize(1).fluent().transform(toId()).toList());
+		emf.close();
+	}
+	
+
+	private void insertDocuments(EntityManager em) {
 		em.getTransaction().begin();
 		em.persist(new Document("a"));
 		em.persist(new Document("b"));
 		em.persist(new Document("c"));
 		em.getTransaction().commit();
-		TypedQuery<Document> q = em.createQuery("from Document order by id",
-				Document.class);
-		assertEquals(3, TypedQueryIterator.query(q).fluent().size());
-		assertEquals("a", TypedQueryIterator.query(q).iterator().next().id);
-		assertEquals(Lists.newArrayList("a", "b", "c"), TypedQueryIterator
-				.query(q).fluent().transform(Document.toId()).toList());
 	}
 
 	private EntityManagerFactory emf() {
