2419b8793f2a6b5cc3b56dd7f3cd1845435e5e80
Dave Moten <davidmoten@gmail.com>
2014-01-25 13:50:21 +1100

    add tests

--- src/main/java/com/github/davidmoten/fjpa/Iterators.java
+++ src/main/java/com/github/davidmoten/fjpa/Iterators.java
@@ -3,14 +3,29 @@ package com.github.davidmoten.fjpa;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-public class Iterators {
+import com.google.common.annotations.VisibleForTesting;
 
+public final class Iterators {
+
+	private Iterators() {
+		//prevent instantiation
+	}
+	
+	@VisibleForTesting
+	void instantiateForTesting() {
+		new Iterators();
+	}
+	
 	public static <R> QueryIterator.Builder<R> query(Query query, Class<R> cls) {
 		return new QueryIterator.Builder<R>(query, cls);
 	}
 
 	public static <R> TypedQueryIterator.Builder<R> query(TypedQuery<R> query) {
 		return new TypedQueryIterator.Builder<R>(query);
+	}
+
+	public static void instantiateForCoverage() {
+		
 	}
 
 }
