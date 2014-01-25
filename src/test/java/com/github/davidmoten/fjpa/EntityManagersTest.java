e6620e4bf9a38443fa5d5e4660feb1b5089db404
Dave Moten <davidmoten@gmail.com>
2014-01-25 13:58:09 +1100

    add tests

---
+++ src/test/java/com/github/davidmoten/fjpa/EntityManagersTest.java
@@ -0,0 +1,33 @@
+package com.github.davidmoten.fjpa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+
+public class EntityManagersTest {
+
+	@Test
+	public void getFullCoverage() {
+		EntityManagers.instantiateForCoverage();
+	}
+
+	@Test
+	public void testEnrichEntityManager() {
+		EntityManager em = EasyMock.createMock(EntityManager.class);
+		assertEquals(em, EntityManagers.enrich(em).get());
+	}
+
+	@Test
+	public void testEnrichEntityManagerWithProperties() {
+		Map<String, Object> map = Maps.newHashMap();
+		assertNotNull(EntityManagers.emf("test",map));
+	}
+}
